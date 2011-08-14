fca088600c7babfa479482851c2d6b732a16dc07
SND\nscoder_cp <SND\nscoder_cp@1eac2b61-d0d7-4846-87ee-9410fe51cbb3>
2011-08-14 20:11:26 +0000

    Checkin - 400
    git-svn-id: https://newgen.svn.codeplex.com/svn@7613 1eac2b61-d0d7-4846-87ee-9410fe51cbb3

---
+++ Mosaic Windows 8 Edition/Widgets/Weather/Settings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Mosaic.Base;
+
+namespace Weather
+{
+    public class Settings : XmlSerializable
+    {
+        public Settings()
+        {
+            TempScale = TemperatureScale.Celsius;
+            RefreshInterval = 20;
+            ShowFeelsLike = false;
+        }
+
+        public string LocationCode { get; set; } 
+        public double RefreshInterval { get; set; }
+        public TemperatureScale TempScale { get; set; }
+        public bool ShowFeelsLike { get; set; }
+    }
+}
