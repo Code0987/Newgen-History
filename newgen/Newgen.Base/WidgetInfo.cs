aeb9045f807af4b30520ec77cb3cc1f7ae19780a
SND\nscoder_cp <SND\nscoder_cp@1eac2b61-d0d7-4846-87ee-9410fe51cbb3>
2011-08-15 15:24:11 +0000

    git-svn-id: https://newgen.svn.codeplex.com/svn@7657 1eac2b61-d0d7-4846-87ee-9410fe51cbb3

---
+++ newgen/Newgen.Base/WidgetInfo.cs
@@ -0,0 +1,19 @@
+﻿using Newgen.Base;
+
+namespace Newgen
+{
+    public class WidgetInfo : XmlSerializable
+    {
+        public string ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Version { get; set; }
+
+        public string Description { get; set; }
+
+        public string Author { get; set; }
+
+        public string AuthorWeb { get; set; }
+    }
+}
